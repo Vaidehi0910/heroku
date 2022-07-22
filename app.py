38867c3ad7909d2dc46668f65cfd7fd941e8fefe
Vaidehi0910 <69051103+Vaidehi0910@users.noreply.github.com>
2022-07-22 15:46:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,30 @@
 import streamlit as st
+import pandas as pd
 
-st.title("Hello guys")
+st.write("""
+# Odd or even Number
+This app tells whether the input is odd or even
+""")
+
+st.header('User Input Parameters')
+
+def user_input_features():
+  number = st.number_input("Number",min_value=0,max_value=2000000,step=1)
+
+  data={'Number':number}
+  features = pd.DataFrame(data, index=[0])
+  return features
+
+
+df = user_input_features()
+
+st.subheader('User Input parameters')
+st.write(df.to_dict())
+
+if (int(df['Number'])%2==0):
+  ans="Even"
+else:
+  ans="Odd"
+
+st.subheader('The Number is:')
+st.write(ans)
